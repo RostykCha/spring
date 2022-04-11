13a7c63a2f1e7bb4e639c8c4b33e65679010df56
RostikGlob <rostyslav.chabria@globallogic.com>
2022-04-11 19:41:15 +0300

    Initial commit

---
+++ src/main/java/ua/roscha/spring/controllers/FirstController.java
@@ -0,0 +1,79 @@
+package ua.roscha.spring.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class FirstController {
+
+    @GetMapping()
+    public String mainPage(){
+        return "general/main";
+    }
+
+    @GetMapping("/hello")
+    public String helloPage(HttpServletRequest request,
+                            Model model) {
+        String name = request.getParameter("name");
+        String surname = request.getParameter("surname");
+
+        System.out.println("Bye " + name + " " + surname);
+
+        model.addAttribute("message", String.format("Bye %s %s", name, surname));
+
+        return "first/hello";
+    }
+
+    @GetMapping("/goodbye")
+    public String goodbyePage(@RequestParam(value = "name", required = false) String name,
+                              @RequestParam(value = "surname", required = false) String surname,
+                              Model model) {
+
+        System.out.println("Bye " + name + " " + surname);
+        model.addAttribute("message", String.format("Bye %s %s", name, surname));
+
+        return "first/goodbye";
+    }
+
+
+    @GetMapping("/first/calculator")
+    public String calculator(@RequestParam(value = "a", required = false) Integer a,
+                             @RequestParam(value = "b", required = false) Integer b,
+                             @RequestParam(value = "action", required = false) String action,
+                             Model model) {
+
+        String message = String.format("Apply %s for %d and %d", action, a, b);
+
+        model.addAttribute("message", message);
+
+        Double result = 0.0;
+
+        if (action!=null){
+            switch (action) {
+                case "multiplication":
+                    result = (double) (a * b);
+                    break;
+
+                case "addiction":
+                    result = (double) (a + b);
+                    break;
+
+                case "subtraction":
+                    result = (double) (a - b);
+                    break;
+
+                case "division":
+                    result = (double) (a / b);
+                    break;
+            }
+        }
+
+        model.addAttribute("calculationResult", result);
+
+        return "first/calculator";
+    }
+}
---
+++ src/main/java/ua/roscha/spring/dao/PersonDao.java
@@ -0,0 +1,36 @@
+package ua.roscha.spring.dao;
+
+import org.springframework.stereotype.Component;
+import ua.roscha.spring.models.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDao {
+
+    private static int PEOPLE_COUNT;
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+        people.add(new Person(++PEOPLE_COUNT, "Tom"));
+        people.add(new Person(++PEOPLE_COUNT, "Bob"));
+        people.add(new Person(++PEOPLE_COUNT, "Mike"));
+        people.add(new Person(++PEOPLE_COUNT, "Katy"));
+    }
+
+    public List<Person> index() {
+        System.out.println("Return peoples list: " + people);
+        return people;
+    }
+
+    public Person show(int id) {
+        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    }
+
+    public void save(Person person) {
+        person.setId(++PEOPLE_COUNT);
+        people.add(person);
+    }
+}
